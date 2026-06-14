67d0f5d93f1d70b0bdf5f1808a12fb7030ca82da
Stas Persaev <persaev10@mail.ru>
2026-06-14 20:19:53 +0300

    Добавил Message contract класс для межсервисного взаимодействия

---
+++ Shared.Contracts/OrderCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Contracts
+{
+    public class OrderCreatedEvent
+    {
+        public Guid OrderId { get; init; }
+        public string CustomerEmail { get; init; } = string.Empty;
+        public decimal Amount { get; init; }
+        public DateTimeOffset CreatedAt { get; init; }
+    }
+}
